4e30ac5b42062b4e986ebd674c47599f61aaf279
mojogrojo <robinjonathan1993@gmail.com>
2021-03-03 14:52:24 +0400

    test

--- src/components/ConvertDate.js
+++ src/components/ConvertDate.js
@@ -4,3 +4,5 @@ export const ConvertDate = (inputFormat) => {
     var d = new Date(inputFormat)
     return [pad(d.getDate()), pad(d.getMonth() + 1), d.getFullYear()].join('/')
 }
+
+//test for commit
